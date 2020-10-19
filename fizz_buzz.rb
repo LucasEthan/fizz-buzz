0d8d19ae65be16bac155622f0c3b91eec241242f
Lucas Ethan <asmrit1010@gmail.com>
2020-10-19 12:32:07 +0200

    Added data validation

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -7,23 +7,30 @@ end
 def divisible_by_5?(number)
   (number % 5).zero?
 end
+begin
+  print "Enter a number: "
+  num = Integer(gets)
 
-print "Enter a number: "
-num = gets.to_i
-fizzbuzz_array = []
+  if num.negative?
+    puts "You must enter a positive integer"
+  end
 
-num.times do |i|
-  # Because we start counting from 1, add 1
-  current_num = i + 1
+  fizzbuzz_array = []
 
-  entry = ""
-  entry += "Fizz" if divisible_by_3?(current_num)
-  entry += "Buzz" if divisible_by_5?(current_num)
+  num.times do |i|
+    # Because we start counting from 1, add 1
+    current_num = i + 1
 
-  # If the number is divisible neither by 3 nor 5, just use the number
-  entry = current_num.to_s if entry.empty?
+    entry = ""
+    entry += "Fizz" if divisible_by_3?(current_num)
+    entry += "Buzz" if divisible_by_5?(current_num)
 
-  fizzbuzz_array << entry
+    # If the number is divisible neither by 3 nor 5, just use the number
+    entry = current_num.to_s if entry.empty?
+    fizzbuzz_array << entry
+  end
+
+  puts fizzbuzz_array
+rescue ArgumentError
+  puts "You must enter a integer"
 end
-
-puts fizzbuzz_array
