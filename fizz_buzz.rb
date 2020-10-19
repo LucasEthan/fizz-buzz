4fe09f806d3fd3997ab7e1aab8183ac2be886834
Lucas Ethan <asmrit1010@gmail.com>
2020-10-19 13:06:34 +0200

    Refactored to use classes

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,12 +1,6 @@
 #!/usr/bin/env ruby
 
-def divisible_by_3?(number)
-  (number % 3).zero?
-end
-
-def divisible_by_5?(number)
-  (number % 5).zero?
-end
+require_relative "fizz_buzz_class"
 
 begin
   print "Enter a number: "
